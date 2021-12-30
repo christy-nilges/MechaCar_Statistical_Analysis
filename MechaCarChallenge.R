e8e94d75aa6f6a99b7937e12d644b637652b6dc4
christy-nilges <christy.nilges@yahoo.com>
2021-12-30 16:04:19 -0600

    CSV files and RScript file

---
+++ MechaCarChallenge.R
@@ -0,0 +1,10 @@
+library(dplyr)
+
+# Linear Regression to Predict MPG
+# read csv to dataframe
+mechacar_mpg_df <- read.csv(file='./MechaCar_mpg.csv', check.names = F, stringsAsFactors = F)
+head(mechacar_mpg_df)
+# multiple linear regression
+lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mechacar_mpg_df)
+# find the p-value and r-squared
+summary(lm(mpg~vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD, data = mechacar_mpg_df))
